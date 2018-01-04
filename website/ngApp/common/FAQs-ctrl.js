4da5a14eb170dd3aa9edd6ed3a54d10bd9d00066
taylorclark <1504384+TaylorClark@users.noreply.github.com>
2018-01-03 23:11:20 -0600

    Standardize some code styling

--- website/ngApp/common/FAQs-ctrl.js
+++ website/ngApp/common/FAQs-ctrl.js
@@ -34,13 +34,11 @@ var Ally;
             // Hook up the rich text editor
             window.setTimeout(function () {
                 var showErrorAlert = function (reason, detail) {
-                    var msg = '';
-                    if (reason === 'unsupported-file-type') {
+                    var msg = "";
+                    if (reason === "unsupported-file-type")
                         msg = "Unsupported format " + detail;
-                    }
-                    else {
+                    else
                         console.log("error uploading file", reason, detail);
-                    }
                     $('<div class="alert"> <button type="button" class="close" data-dismiss="alert">&times;</button>' +
                         '<strong>File upload error</strong> ' + msg + ' </div>').prependTo('#alerts');
                 };
--- website/ngApp/common/FAQs-ctrl.ts
+++ website/ngApp/common/FAQs-ctrl.ts
@@ -53,12 +53,12 @@ namespace Ally
             {
                 var showErrorAlert = function( reason: string, detail: any )
                 {
-                    var msg = '';
-                    if( reason === 'unsupported-file-type' ) { msg = "Unsupported format " + detail; }
+                    var msg = "";
+                    if( reason === "unsupported-file-type" )
+                        msg = "Unsupported format " + detail;
                     else
-                    {
                         console.log( "error uploading file", reason, detail );
-                    }
+
                     $( '<div class="alert"> <button type="button" class="close" data-dismiss="alert">&times;</button>' +
                         '<strong>File upload error</strong> ' + msg + ' </div>' ).prependTo( '#alerts' );
                 };
