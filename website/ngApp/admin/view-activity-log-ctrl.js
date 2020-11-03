7b71d287f7eb31412054773d77194a6c8f1ae17b
taylorclark <1504384+TaylorClark@users.noreply.github.com>
2020-11-02 19:59:06 -0600

    Allow hiding of login and email messages

--- website/ngApp/admin/view-activity-log-ctrl.js
+++ website/ngApp/admin/view-activity-log-ctrl.js
@@ -15,29 +15,48 @@ var Ally;
         function ViewActivityLogController($http) {
             this.$http = $http;
             this.isLoading = false;
+            this.shouldHideLoginAndEmailMessages = false;
         }
         /**
         * Called on each controller after all the controllers on an element have been constructed
         */
         ViewActivityLogController.prototype.$onInit = function () {
+            this.shouldHideLoginAndEmailMessages = window.localStorage["activityLog_hideLoginAndEmailMessages"] === "true";
             // Initialize the UI
             this.retrieveEntries();
+        };
+        /**
+         * Occurs when the users toggles the login/email filter checkbox
+         */
+        ViewActivityLogController.prototype.onHideLoginAndEmailMessagesChange = function () {
+            window.localStorage["activityLog_hideLoginAndEmailMessages"] = this.shouldHideLoginAndEmailMessages;
+            this.filterMessages();
         };
         /**
          * Load the activity log data
          */
         ViewActivityLogController.prototype.retrieveEntries = function () {
+            var _this = this;
             this.isLoading = true;
-            var innerThis = this;
             this.$http.get("/api/ActivityLog").then(function (logResponse) {
-                innerThis.isLoading = false;
-                innerThis.logEntries = logResponse.data;
+                _this.isLoading = false;
+                _this.allLogEntries = logResponse.data;
                 // The date comes down as a string so let's convert it to a Date object for the local time zone
-                _.each(innerThis.logEntries, function (e) { return e.postDate = moment(e.postDate).toDate(); });
+                _.each(_this.allLogEntries, function (e) { return e.postDate = moment(e.postDate).toDate(); });
+                _this.filterMessages();
             }, function (errorResponse) {
-                innerThis.isLoading = false;
+                _this.isLoading = false;
                 alert("Failed to load activity log: " + errorResponse.data.exceptionMessage);
             });
+        };
+        /**
+         * Update the visible messages based on filter criteria
+         */
+        ViewActivityLogController.prototype.filterMessages = function () {
+            if (this.shouldHideLoginAndEmailMessages)
+                this.filteredLogEntries = _.filter(this.allLogEntries, function (e) { return e.activityMessage !== "Logged in" && e.activityMessage.indexOf("Group email sent") !== 0; });
+            else
+                this.filteredLogEntries = this.allLogEntries;
         };
         ViewActivityLogController.$inject = ["$http"];
         return ViewActivityLogController;
--- website/ngApp/admin/view-activity-log-ctrl.ts
+++ website/ngApp/admin/view-activity-log-ctrl.ts
@@ -18,7 +18,9 @@
     {
         static $inject = ["$http"];
         isLoading: boolean = false;
-        logEntries: ActivityLogEntry[];
+        allLogEntries: ActivityLogEntry[];
+        filteredLogEntries: ActivityLogEntry[];
+        shouldHideLoginAndEmailMessages: boolean = false;
 
 
         /**
@@ -34,8 +36,20 @@
         */
         $onInit()
         {
+            this.shouldHideLoginAndEmailMessages = window.localStorage["activityLog_hideLoginAndEmailMessages"] === "true";
+
             // Initialize the UI
             this.retrieveEntries();
+        }
+
+
+        /**
+         * Occurs when the users toggles the login/email filter checkbox
+         */
+        onHideLoginAndEmailMessagesChange()
+        {
+            window.localStorage["activityLog_hideLoginAndEmailMessages"] = this.shouldHideLoginAndEmailMessages;
+            this.filterMessages();
         }
 
 
@@ -46,20 +60,35 @@
         {
             this.isLoading = true;
 
-            var innerThis = this;
-            this.$http.get( "/api/ActivityLog" ).then(( logResponse: ng.IHttpPromiseCallbackArg<ActivityLogEntry[]> ) =>
-            {
-                innerThis.isLoading = false;
-                innerThis.logEntries = logResponse.data;
+            this.$http.get( "/api/ActivityLog" ).then(
+                ( logResponse: ng.IHttpPromiseCallbackArg<ActivityLogEntry[]> ) =>
+                {
+                    this.isLoading = false;
+                    this.allLogEntries = logResponse.data;
 
-                // The date comes down as a string so let's convert it to a Date object for the local time zone
-                _.each( innerThis.logEntries, ( e ) => e.postDate = moment(( e as any ).postDate ).toDate() );
-                
-            }, ( errorResponse: ng.IHttpPromiseCallbackArg<Ally.ExceptionResult> ) =>
-            {
-                innerThis.isLoading = false;
-                alert( "Failed to load activity log: " + errorResponse.data.exceptionMessage );
-            } );
+                    // The date comes down as a string so let's convert it to a Date object for the local time zone
+                    _.each( this.allLogEntries, ( e ) => e.postDate = moment( ( e as any ).postDate ).toDate() );
+
+                    this.filterMessages();
+                },
+                ( errorResponse: ng.IHttpPromiseCallbackArg<Ally.ExceptionResult> ) =>
+                {
+                    this.isLoading = false;
+                    alert( "Failed to load activity log: " + errorResponse.data.exceptionMessage );
+                }
+            );
+        }
+
+
+        /**
+         * Update the visible messages based on filter criteria
+         */
+        filterMessages()
+        {
+            if( this.shouldHideLoginAndEmailMessages )
+                this.filteredLogEntries = _.filter( this.allLogEntries, e => e.activityMessage !== "Logged in" && e.activityMessage.indexOf( "Group email sent" ) !== 0 );
+            else
+                this.filteredLogEntries = this.allLogEntries;
         }
     }
 }
