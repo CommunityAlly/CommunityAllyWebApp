4274bcb94da055a170f82b4861040d2df42ba02f
taylorclark <1504384+TaylorClark@users.noreply.github.com>
2022-11-27 12:42:10 -0700

    Make it more clear if someone should switch sites

--- website/ngApp/chtn/public/hoa-sign-up-wizard.js
+++ website/ngApp/chtn/public/hoa-sign-up-wizard.js
@@ -73,7 +73,7 @@ var Ally;
             }
             var shouldShowCondoMessage = this.signUpInfo.name.toLowerCase().indexOf("condo") !== -1;
             if (shouldShowCondoMessage && !this.shouldShowCondoMessage)
-                $("#suggestCondoMessageLabel").css("font-size", "1.3em").css("margin", "25px auto").fadeIn(200).fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200);
+                $("#suggestCondoMessageLabel").css("font-size", "1.3em").css("margin", "25px auto").addClass("alert alert-warning").fadeIn(200).fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200).fadeIn(200).fadeOut(200).fadeIn(200);
             this.shouldShowCondoMessage = shouldShowCondoMessage;
         };
         /**
