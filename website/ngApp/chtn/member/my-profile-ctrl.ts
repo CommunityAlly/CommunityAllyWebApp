057633fca85f2b6db828de44945b81eca20ca1a7
taylorclark <1504384+TaylorClark@users.noreply.github.com>
2026-07-26 08:00:16 -0600

    Support for enabling SMS in profile

--- website/ngApp/chtn/member/my-profile-ctrl.ts
+++ website/ngApp/chtn/member/my-profile-ctrl.ts
@@ -14,6 +14,7 @@ namespace Ally
         showPhoneInMeetNeighbors: boolean;
         postmarkReportedBadEmailUtc: Date;
         hasSmsConsent: boolean | null;
+        smsReceiveLevel: "emergency" | "notification" | null;
         hasEmail: boolean;
     }
 
@@ -35,6 +36,7 @@ namespace Ally
         mfaMethodsCsv: string;
         phoneVerificationDateUtc: Date;
         postmarkReportedBadEmailReason: string;
+        smsReceiveLevel: "emergency" | "notification" | null;
     }
 
     export class PtaMember extends SimpleUserEntry
